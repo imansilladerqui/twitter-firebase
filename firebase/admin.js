d482fecab8980bdb6689a8b507f27aa2e7aa16db
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-27 17:27:15 +0200

    firebase-keys hide

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-keys.json");
+const serviceAccount = process.env.FIREBASE_KEYS;
 
 try {
   admin.initializeApp({
