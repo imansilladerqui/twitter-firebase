6d1d3d79c9e0be833af0efc3b032fd2f1edb0a65
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-28 09:53:32 +0200

    changing server address

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -13,7 +13,7 @@ export async function getServerSideProps(context) {
   const { params, res } = context;
   const { id } = params;
 
-  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`);
+  const apiResponse = await fetch(`/api/devits/${id}`);
   if (apiResponse.ok) {
     const props = await apiResponse.json();
     return { props };
