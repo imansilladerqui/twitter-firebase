43ad06e05bfd2ec604fb5a9fe81b63e4f79a1f09
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-28 19:50:10 +0200

    building

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,17 +1,10 @@
-import isInitialize from "./isInitialize.js";
-
 const admin = require("firebase-admin");
 
-console.log(isInitialize.name);
-console.log(isInitialize.database());
+const serviceAccount = require("./firebase-keys.json");
 
 try {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
-      project_id: "twitts-69317",
-    }),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 } catch (e) {}
