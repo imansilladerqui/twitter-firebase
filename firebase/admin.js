0b13f3656d558d2355f90bf8e6ab4a95ec989c14
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-28 19:21:36 +0200

    building

--- firebase/admin.js
+++ firebase/admin.js
@@ -15,7 +15,16 @@ try {
   });
 } catch (e) {}
 
-const config = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
+const config = {
+  apiKey: "AIzaSyBOyj8ld1sLhEEk0i2kCZ8Wg5gigip27Qo",
+  authDomain: "twitts-69317.firebaseapp.com",
+  databaseURL:
+    "https://twitts-69317-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "twitts-69317",
+  storageBucket: "twitts-69317.appspot.com",
+  messagingSenderId: "86737244928",
+  appId: "1:86737244928:web:005f1f0ddb042487bc19e5",
+};
 
 !admin.apps.length && admin.initializeApp(config);
 
