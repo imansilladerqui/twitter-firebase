11bd92862f1eccccee8dd56541cbaf55f5937bbe
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2022-06-28 14:45:44 +0200

    hide keys

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,8 +1,14 @@
 const admin = require("firebase-admin");
 
+// const serviceAccount = require("./firebase-keys.json");
+
 try {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      project_id: "twitts-69317",
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 } catch (e) {}
